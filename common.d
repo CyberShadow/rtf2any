fdd9700d7281af3f4d4bf80dbeddb512151d248e
Vladimir Panteleev <vladimir@thecybershadow.net>
2011-02-24 10:49:36 +0200

    common: add Block.toString

--- common.d
+++ common.d
@@ -1,5 +1,7 @@
 module rtf2any.common;
 
+import std.string;
+
 enum BlockType { Text, NewParagraph, PageBreak }
 
 struct BlockAttr
@@ -8,6 +10,18 @@ struct BlockAttr
 	int listLevel;
 	int fontSize;
 	int fontColor;
+
+	string toString()
+	{
+		string[] attrs;
+		if (bold) attrs ~= "bold";
+		if (italic) attrs ~= "italic";
+		if (underline) attrs ~= "underline";
+		if (listLevel) attrs ~= format("listLevel=%d", listLevel);
+		if (fontSize) attrs ~= format("fontSize=%d", fontSize);
+		if (fontColor) attrs ~= format("fontColor=%d", fontColor);
+		return "[" ~ join(attrs, " ") ~ "]";
+	}
 }
 
 struct Block
@@ -15,4 +29,18 @@ struct Block
 	BlockType type;
 	BlockAttr attr;
 	string text;
+
+	string toString()
+	{
+		string s = attr.toString;
+		final switch (type)
+		{
+		case BlockType.Text:
+			return s ~ ` Text: ` ~ text;
+		case BlockType.NewParagraph:
+			return s ~ ` NewParagraph`;
+		case BlockType.PageBreak:
+			return s ~ ` PageBreak`;
+		}
+	}
 }
