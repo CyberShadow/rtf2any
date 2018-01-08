0d4fffc5146b4f8460068df26db6437596b0e13d
Vladimir Panteleev <git@thecybershadow.net>
2018-01-08 06:33:38 +0000

    rtf2any.rtf.parser: Add a TODO

--- rtf/parser.d
+++ rtf/parser.d
@@ -252,7 +252,7 @@ struct Parser
 					preAppend();
 					BlockAttr parAttr = attr;
 					// discard some attributes for endlines
-					parAttr.bold = parAttr.italic = parAttr.underline = false;
+					parAttr.bold = parAttr.italic = parAttr.underline = false; // TODO: these shouldn't be discarded!
 					parAttr.subSuper = SubSuper.none;
 					parAttr.fontColor = defaultColor;
 					parAttr.columnIndex = -1;
