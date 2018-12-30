17082ad7e3438b9071d9e862a13ca2a48dbc0e54
David Ellsworth <david@worms2d.info>
2018-12-30 15:29:42 -0800

    rtf2any.formatter.bbcode: Fix this module to adapt to the restructuring
    This is probably not 100% correctly done, but it works.

--- formatter/bbcode.d
+++ formatter/bbcode.d
@@ -7,24 +7,33 @@ class BBCodeFormatter : NestedFormatter
 {
 	this(Block[] blocks) { super(blocks); }
 
-	bool inList, bulletPending;
+	bool bulletPending;
+	int listLevel;
 
-	void pre() { if (bulletPending) s ~= "[*]", bulletPending = false; }
+	void pre()
+	{
+		if (bulletPending)
+		{
+			if (listLevel != 0)
+				s ~= "[*]";
+			bulletPending = false;
+		}
+	}
 
 	override void addText(string text) { pre(); s ~= text; }
-	override void newParagraph() { s ~= "\n"; if (inList) bulletPending = true; }
+	override void newParagraph() { s ~= "\n"; if (listLevel != 0) bulletPending = true; }
 
-	override void addBold() { pre(); s ~= "[B]"; }
-	override void addItalic() { pre(); s ~= "[I]"; }
-	override void addUnderline() { pre(); s ~= "[U]"; }
-	override void addListLevel(int level) { s ~= "[LIST]"; inList = bulletPending = true; }
+	override void addBold(bool enabled) { pre(); s ~= enabled ? "[B]" : "[/B]"; }
+	override void addItalic(bool enabled) { pre(); s ~= enabled ? "[I]" : "[/I]"; }
+	override void addUnderline(bool enabled) { pre(); s ~= enabled ? "[U]" : "[/U]"; }
+	override void addIndent(int left, int firstLine, bool list) { s ~= "[LIST]"; bulletPending = true; listLevel++; }
 	override void addFontSize(int size) { assert(0, "TODO"); }
 	override void addFontColor(int color) { assert(0, "TODO"); }
 	
-	override void removeBold() { s ~= "[/B]"; }
-	override void removeItalic() { s ~= "[/I]"; }
-	override void removeUnderline() { s ~= "[/U]"; }
-	override void removeListLevel(int level) { s ~= "[/LIST]"; if (level==1) inList = bulletPending = false; }
+	override void removeBold(bool enabled) { pre(); s ~= enabled ? "[/B]" : "[B]"; }
+	override void removeItalic(bool enabled) { pre(); s ~= enabled ? "[/I]" : "[I]"; }
+	override void removeUnderline(bool enabled) { pre(); s ~= enabled ? "[/U]" : "[U]"; }
+	override void removeIndent(int left, int firstLine, bool list) { s ~= "[/LIST]"; if (--listLevel == 0) bulletPending = false; }
 	override void removeFontSize(int size) { assert(0, "TODO"); }
 	override void removeFontColor(int color) { assert(0, "TODO"); }
 }
