c1080f73e84055af834e6836d49066738c53160f
David Ellsworth <david@worms2d.info>
2018-12-30 14:26:21 -0800

    rtf2any.formatter.mediawiki: Fix this module to adapt to the restructuring
    This is probably not 100% correctly done.

--- formatter/mediawiki.d
+++ formatter/mediawiki.d
@@ -32,20 +32,17 @@ class MediaWikiFormatter : NestedFormatter
 	override void addText(string text) { pre(); if (inTable) text = text.replace("\t", " || "); s ~= text.replace("<", "&lt;").replace(">", "&gt;").replace("{{", "<nowiki>{{</nowiki>").replace("}}", "<nowiki>}}</nowiki>"); }
 	override void newParagraph() { if (!inTable) s ~= "\n"; else s ~= "\n|-\n| "; if (listLevel) bulletPending = true; }
 
-	override void addBold() { pre(); s ~= "'''"; }
-	override void addItalic() { pre(); s ~= "''"; }
-	override void addUnderline() { pre(); s ~= "<u>"; }
-	override void addListLevel(int level) { listLevel = level; bulletPending = true; }
+	override void addBold(bool enabled) { pre(); s ~= "'''"; }
+	override void addItalic(bool enabled) { pre(); s ~= "''"; }
+	override void addUnderline(bool enabled) { pre(); s ~= enabled ? "<u>" : "</u>"; }
+	override void addIndent(int left, int firstLine, bool list) { listLevel = left; bulletPending = true; }
 	override void addFontSize(int size) { pre(); if (size > 25 && paraStart) s ~= "== "; else if (size > 20 && paraStart) s ~= "=== "; else if (size < 20) s ~= "<small>"; }
 	override void addFontColor(int color) { pre(); s ~= .format(`<span style="color: #%06x">`, color); }
-	override void addTabCount(int tabCount) { if (listLevel==0) { inTable = true; s ~= "{|\n| "; } }
+	override void addTabs(int[] tabs) { if (listLevel==0) { inTable = true; s ~= "{|\n| "; } }
 	
-	override void removeBold() { s ~= "'''"; }
-	override void removeItalic() { s ~= "''"; }
-	override void removeUnderline() { s ~= "</u>"; }
-	override void removeListLevel(int level) { listLevel = level-1; }
+	override void removeIndent(int left, int firstLine, bool list) { listLevel = left-1; }
 	override void removeFontSize(int size) { if (size > 25 && paraEnd) s ~= " =="; else if (size > 20 && paraEnd) s ~= " ==="; else if (size < 20) s ~= "</small>"; }
 	override void removeFontColor(int color) { s ~= "</span>"; }
-	override void removeTabCount(int tabCount) { if (inTable) { inTable = false; s = s[0..$-5] ~ "|}\n"; } }
+	override void removeTabs(int[] tabs) { if (inTable) { inTable = false; s = s[0..$-5] ~ "|}\n"; } }
 }
 
