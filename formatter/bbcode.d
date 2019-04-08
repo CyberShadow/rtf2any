faa1e683ad0ecd9e12dc6125580989bb3e9c06d8
Vladimir Panteleev <git@thecybershadow.net>
2019-04-08 00:34:16 +0000

    rtf2any.formatter.bbcode: Do not attempt to disable overlapping tags
    At least on tus-wa.com, e.g.
    "Normal[B]Bold[I]BoldItalic[/B]Italic[/I]Normal" does not render
    as expected.
    
    The flat-to-nested conversion algorithm attempts to avoid emitting
    nested format-negating instructions as much as possible. Situations
    where they are required are not encountered in current versions of the
    Readme changelog.

--- formatter/bbcode.d
+++ formatter/bbcode.d
@@ -1,5 +1,7 @@
 module rtf2any.formatter.bbcode;
 
+import std.exception;
+
 import rtf2any.common;
 import rtf2any.formatter.nested;
 
@@ -23,16 +25,18 @@ class BBCodeFormatter : NestedFormatter
 	override void addText(string text) { pre(); s ~= text; }
 	override void newParagraph() { s ~= "\n"; if (listLevel != 0) bulletPending = true; }
 
-	override void addBold(bool enabled) { pre(); s ~= enabled ? "[B]" : "[/B]"; }
-	override void addItalic(bool enabled) { pre(); s ~= enabled ? "[I]" : "[/I]"; }
-	override void addUnderline(bool enabled) { pre(); s ~= enabled ? "[U]" : "[/U]"; }
+	private static void checkEnable(bool enabled) { enforce(enabled, "Can not disable formatting in BBCode"); }
+
+	override void addBold(bool enabled) { checkEnable(enabled); pre(); s ~= "[B]"; }
+	override void addItalic(bool enabled) { checkEnable(enabled); pre(); s ~= "[I]"; }
+	override void addUnderline(bool enabled) { checkEnable(enabled); pre(); s ~= "[U]"; }
 	override void addIndent(int left, int firstLine, bool list) { s ~= "[LIST]"; bulletPending = true; listLevel++; }
 	override void addFontSize(int size) { assert(0, "TODO"); }
 	override void addFontColor(int color) { assert(0, "TODO"); }
 	
-	override void removeBold(bool enabled) { pre(); s ~= enabled ? "[/B]" : "[B]"; }
-	override void removeItalic(bool enabled) { pre(); s ~= enabled ? "[/I]" : "[I]"; }
-	override void removeUnderline(bool enabled) { pre(); s ~= enabled ? "[/U]" : "[U]"; }
+	override void removeBold(bool enabled) { checkEnable(enabled); pre(); s ~= "[/B]"; }
+	override void removeItalic(bool enabled) { checkEnable(enabled); pre(); s ~= "[/I]"; }
+	override void removeUnderline(bool enabled) { checkEnable(enabled); pre(); s ~= "[/U]"; }
 	override void removeIndent(int left, int firstLine, bool list) { s ~= "[/LIST]"; if (--listLevel == 0) bulletPending = false; }
 	override void removeFontSize(int size) { assert(0, "TODO"); }
 	override void removeFontColor(int color) { assert(0, "TODO"); }
