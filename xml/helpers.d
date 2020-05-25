ab4bec6ebd557575f00a2a36f0705424e49e4eba
Vladimir Panteleev <git@thecybershadow.net>
2020-05-25 19:30:17 +0000

    rtf2any.xml.helpers: Add describeNode

--- xml/helpers.d
+++ xml/helpers.d
@@ -2,6 +2,7 @@ module rtf2any.xml.helpers;
 
 import std.algorithm.iteration;
 import std.array;
+import std.conv;
 
 import ae.utils.aa;
 import ae.utils.xmllite;
@@ -60,4 +61,20 @@ XmlNode newNode(string tag, string[string] attributes = null, XmlNode[] children
 XmlNode newTextNode(string text)
 {
 	return newNode(XmlNodeType.Text, text);
+}
+
+// For error messages
+string describeNode(XmlNode node)
+{
+	switch (node.type)
+	{
+		case XmlNodeType.Node:
+			return "<" ~ node.tag ~ ">";
+		case XmlNodeType.Meta:
+			return "<?" ~ node.tag ~ "?>";
+		case XmlNodeType.Text:
+			return "text node (\"" ~ node.tag ~ "\")";
+		default:
+			return node.type.to!string;
+	}
 }
